6e348376a85ba8c379ea799c7ec9dccacb16288d
Moayad-B <73670997+Moayad-B@users.noreply.github.com>
2020-12-05 21:46:23 -0800

    Add files via upload

---
+++ PopulateProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class PopulateProfile : MonoBehaviour
+{
+    Profile Profile;
+    public TextMeshProUGUI name ;
+    public TextMeshProUGUI total;
+    public TextMeshProUGUI wins;
+    public TextMeshProUGUI loses;
+    private int totalinteger;
+
+    // Start is called before the first frame update
+    void Start() {
+        Profile = ClientLibrary.getProfile(userInfo.userName); ;
+        name.text = Profile.Username;
+        wins.text = "Wins: " +  Profile.Wins.ToString() ;
+        loses.text = "Losses: " + Profile.Losses.ToString();
+        totalinteger = Profile.Wins + Profile.Losses;
+        total.text = "Total: " + totalinteger.ToString();
+
+    }
+
+
+
+
+}
