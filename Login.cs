6dc5c4ffda53d932478bc35a28292a25d31fa742
Moayad-B <73670997+Moayad-B@users.noreply.github.com>
2020-12-05 19:40:25 -0800

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Login : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField userName;
+    public InputField password;
+    public Button submit;
+
+    public void LoginUser()
+    {
+        ClientLibrary.authUser(userName.text, password.text);
+    }
+
+}
